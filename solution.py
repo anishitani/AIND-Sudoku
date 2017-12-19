4f4032ec8857be22c099a641671d14f1b7a05889
André Nishitani <atoshio25@gmail.com>
2017-12-19 03:51:53 -0200

    First project solution

--- solution.py
+++ solution.py
@@ -6,10 +6,9 @@ from collections import Counter
 row_units = [cross(r, cols) for r in rows]
 column_units = [cross(rows, c) for c in cols]
 square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
-unitlist = row_units + column_units + square_units
-
-# TODO: Update the unit list to add the new diagonal units
-unitlist = unitlist
+diagonal_units = [ ['A1', 'B2', 'C3', 'D4', 'E5', 'F6', 'G7', 'H8', 'I9'],['A9', 'B8', 'C7', 'D6', 'E5', 'F4', 'G3', 'H2', 'I1'] ]
+unitlist = row_units + column_units + square_units + diagonal_units
+print(unitlist)
 
 units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
 peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
